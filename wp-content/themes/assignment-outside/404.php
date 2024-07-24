80f233976138d4dcb494ba504277d3c96b740f5f
Amit MaharXan <amit.maharxan@gmail.com>
2024-07-24 10:15:52 +0545

    added 404

--- wp-content/themes/assignment-outside/404.php
+++ wp-content/themes/assignment-outside/404.php
@@ -1,3 +1,24 @@
 <?php do_action('outside_header'); ?>
 
+<div class="page-title" data-aos="fade" style="background-image: url(<?php echo site_url('/wp-content/uploads/2024/07/error.jpg');?>);">
+    <div class="container position-relative">
+            <h1 class="title-1">404</h1>
+            <h2 class="title-2">Page Not Found</h2>
+    </div>
+</div>
+
+<section id="error" class="error section">
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row">
+          <div class="col-md-12">
+            <a class="back-homepage" href="<?php echo site_url();?>">BACK TO HOMEPAGE</a>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
 <?php do_action('outside_footer'); ?>
---
+++ wp-content/themes/assignment-outside/readme.txt
@@ -0,0 +1,19 @@
+=== Assignment Outside ===
+
+Contributors: Amit Maharjan
+Requires at least: 6.4
+Tested up to: 6.6
+Requires PHP: 7.0
+
+== Description ==
+
+Assignment Outside theme is only built for assignment tasks.
+
+Theme template is used from BootstrapMade.
+All images are used from google.
+
+Pages includes homepage, events listing page, events details page, faq page, contact page & privacy policy page.
+
+Assignment Outside theme is for lisiting the events.
+All events are filtered and paginated using ajax.
+Contact form 7 is used for contact form.
